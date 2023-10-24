9d3794495a6d87fe90fad1f41650e823eb4f6dd4
Jeff Michael Gomez <jeffmichael.gomez@telus.com>
2023-10-24 22:25:12 +0800

    feat: add bq pusher

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import * as core from '@actions/core'
+import { BigQuery } from '@google-cloud/bigquery'
+
+const totalCrq = core.getInput('totalCrq');
+const tableId = core.getInput('tableId');
+const datasetId = core.getInput('datasetId');
+const projectId = core.getInput('projectId');
+
+async function getCRQResult(totalCrq, tableId, datasetId, projectId) {
+  try {
+    const payload = {
+      ingestion_time: moment.utc().format("YYYY-MM-DD HH:mm:ss.SSSSSS"),
+      totalCrq: totalCrq,
+    };
+
+    const bigquery = new BigQuery({ projectId })
+
+    const save = await bigquery
+      .dataset(datasetId)
+      .table(tableId)
+      .insert([payload]);
+
+    if (save.length > 0) {
+      return "strategies inserted successfully";
+    }
+  } catch (error) {
+    return console.error(`Error getting feature: ${error}`);
+  }
+}
+
+getCRQResult(totalCrq, tableId, datasetId, projectId);
